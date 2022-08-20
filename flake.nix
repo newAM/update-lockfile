2626ad0523ddaf290321f9b502d2b0fa19ea288b
Alex Martens <alex@thinglab.org>
2022-08-20 15:29:01 -0700

    nixpkgs: master -> unstable-small

--- flake.nix
+++ flake.nix
@@ -2,8 +2,7 @@
   description = "Lockfile updaters";
 
   inputs = {
-    # temporarily using master for aarch64-darwin fixes
-    nixpkgs.url = "github:nixos/nixpkgs/master";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
