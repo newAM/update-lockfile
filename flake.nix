34b2ecae51513cb6af17aee5d3971b322010c906
Alex Martens <alex@thinglab.org>
2022-11-11 08:39:11 -0800

    flake.lock: update
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/448a599c49978c2794401bfc3a2e1fba1a8663be' (2022-10-28)
      → 'github:NixOS/nixpkgs/872fceeed60ae6b7766cc0a4cd5bf5901b9098ec' (2022-11-09)

--- flake.nix
+++ flake.nix
@@ -23,9 +23,6 @@
         inherit system;
         overlays = [overlay];
       };
-
-    nixSrc = nixpkgs.lib.sources.sourceFilesBySuffices ./. [".nix"];
-    pySrc = nixpkgs.lib.sources.sourceFilesBySuffices ./. [".py"];
   in {
     overlays = {
       default = overlay;
@@ -54,41 +51,45 @@
       }
     );
 
-    checks = forEachSystem (
-      system: let
-        pkgs = importPkgs system;
-      in {
-        inherit (pkgs) update-lockfile;
+    checks = let
+      nixSrc = nixpkgs.lib.sources.sourceFilesBySuffices ./. [".nix"];
+      pySrc = nixpkgs.lib.sources.sourceFilesBySuffices ./. [".py"];
+    in
+      forEachSystem (
+        system: let
+          pkgs = importPkgs system;
+        in {
+          inherit (pkgs) update-lockfile;
 
-        black = pkgs.runCommand "black" {} ''
-          ${pkgs.python3Packages.black}/bin/black ${pySrc}
-          touch $out
-        '';
-
-        flake8 =
-          pkgs.runCommand "flake8"
-          {
-            buildInputs = with pkgs.python3Packages; [
-              flake8
-              flake8-bugbear
-              pep8-naming
-            ];
-          }
-          ''
-            flake8 --max-line-length 88 ${pySrc}
+          black = pkgs.runCommand "black" {} ''
+            ${pkgs.python3Packages.black}/bin/black ${pySrc}
             touch $out
           '';
 
-        alejandra = pkgs.runCommand "alejandra" {} ''
-          ${pkgs.alejandra}/bin/alejandra --check ${nixSrc}
-          touch $out
-        '';
+          flake8 =
+            pkgs.runCommand "flake8"
+            {
+              buildInputs = with pkgs.python3Packages; [
+                flake8
+                flake8-bugbear
+                pep8-naming
+              ];
+            }
+            ''
+              flake8 --max-line-length 88 ${pySrc}
+              touch $out
+            '';
 
-        statix = pkgs.runCommand "statix" {} ''
-          ${pkgs.statix}/bin/statix check ${nixSrc}
-          touch $out
-        '';
-      }
-    );
+          alejandra = pkgs.runCommand "alejandra" {} ''
+            ${pkgs.alejandra}/bin/alejandra --check ${nixSrc}
+            touch $out
+          '';
+
+          statix = pkgs.runCommand "statix" {} ''
+            ${pkgs.statix}/bin/statix check ${nixSrc}
+            touch $out
+          '';
+        }
+      );
   };
 }
